d1c570c1d83d4a4e5aff55662dfb1b05486e41d3
Tobias Walter <tobias.walter@gmx.net>
2014-11-06 08:45:19 +0100

    readded icon, reenabled code analysis in debug mode

--- StandUpTimer/Properties/AssemblyInfo.cs
+++ StandUpTimer/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyVersion("0.6.1106.45")]
-[assembly: AssemblyFileVersion("0.6.1106.45")]
+[assembly: AssemblyVersion("0.6.1106.53")]
+[assembly: AssemblyFileVersion("0.6.1106.53")]
 
 [assembly: AssemblyMetadata("SquirrelAwareVersion", "1")]
 
