a78227e0d852f27d25dc3df44886b6e9fffc035f
Tobias Walter <tobias.walter@gmx.net>
2015-04-09 15:10:08 +0200

    created new statistics view with model and controller

---
+++ StandUpTimer.Web/Controllers/StatisticsController.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using StandUpTimer.Web.Models;
+
+namespace StandUpTimer.Web.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        private readonly StatusContext _db = new StatusContext();
+
+        // GET: Statistics
+        public ActionResult Index()
+        {
+            return View(_db.Statuses.ToList());
+        }
+    }
+}
---
+++ StandUpTimer.Web/Models/Status.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace StandUpTimer.Web.Models
+{
+    public class Status
+    {
+        public int Id { get; set; }
+        public DateTime DateTime { get; set; }
+        public Position Position { get; set; }
+    }
+
+    public enum Position
+    {
+        Standing,
+        Sitting,
+        Inactive
+    }
+
+    public class StatusContext : DbContext
+    {
+        public DbSet<Status> Statuses { get; set; }
+    }
+}
