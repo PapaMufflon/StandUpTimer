d38c53cc830360f5b208724da1e5d6d08ba88a87
Tobias Walter <tobias.walter@gmx.net>
2015-04-09 15:30:06 +0200

    added temporary sample data

--- StandUpTimer.Web/Controllers/StatisticsController.cs
+++ StandUpTimer.Web/Controllers/StatisticsController.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 using StandUpTimer.Web.Models;
 
@@ -11,7 +13,78 @@ namespace StandUpTimer.Web.Controllers
         // GET: Statistics
         public ActionResult Index()
         {
-            return View(_db.Statuses.ToList());
+            var statuses = _db.Statuses.ToList();
+            statuses.AddRange(new List<Status>
+            {
+                new Status
+                {
+                    DateTime = new DateTime(2015, 4, 9, 8, 15, 0),
+                    Position = Position.Sitting
+                },
+                new Status
+                {
+                    DateTime = new DateTime(2015, 4, 9, 9, 17, 0),
+                    Position = Position.Standing
+                },
+                new Status
+                {
+                    DateTime = new DateTime(2015, 4, 9, 9, 37, 0),
+                    Position = Position.Sitting
+                },
+                new Status
+                {
+                    DateTime = new DateTime(2015, 4, 9, 11, 0, 0),
+                    Position = Position.Standing
+                },
+                new Status
+                {
+                    DateTime = new DateTime(2015, 4, 9, 11, 20, 0),
+                    Position = Position.Sitting
+                },
+                new Status
+                {
+                    DateTime = new DateTime(2015, 4, 9, 12, 15, 0),
+                    Position = Position.Inactive
+                },
+
+                new Status
+                {
+                    DateTime = new DateTime(2015, 4, 9, 12, 50, 0),
+                    Position = Position.Sitting
+                },
+
+                new Status
+                {
+                    DateTime = new DateTime(2015, 4, 9, 14, 0, 0),
+                    Position = Position.Standing
+                },
+
+                new Status
+                {
+                    DateTime = new DateTime(2015, 4, 9, 14, 20, 0),
+                    Position = Position.Sitting
+                },
+
+                new Status
+                {
+                    DateTime = new DateTime(2015, 4, 9, 15, 25, 0),
+                    Position = Position.Standing
+                },
+
+                new Status
+                {
+                    DateTime = new DateTime(2015, 4, 9, 15, 45, 0),
+                    Position = Position.Sitting
+                },
+
+                new Status
+                {
+                    DateTime = new DateTime(2015, 4, 9, 17, 5, 0),
+                    Position = Position.Inactive
+                }
+            });
+
+            return View(statuses);
         }
     }
 }
