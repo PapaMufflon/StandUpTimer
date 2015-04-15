6a2a676303d996e2526c8d11787f14ebac807d3d
Tobias Walter <tobias.walter@gmx.net>
2015-04-15 12:19:05 +0200

    give the cc text a little more space

--- StandUpTimer/Properties/AssemblyInfo.cs
+++ StandUpTimer/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyVersion("0.6.0415.41")]
-[assembly: AssemblyFileVersion("0.6.0415.41")]
+[assembly: AssemblyVersion("0.6.0415.44")]
+[assembly: AssemblyFileVersion("0.6.0415.44")]
 
 [assembly: AssemblyMetadata("SquirrelAwareVersion", "1")]
 
