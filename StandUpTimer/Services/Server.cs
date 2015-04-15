c04e9bdc26502d9c6a61758979b7ec2f1c048078
Tobias Walter <tobias.walter@gmx.net>
2015-04-15 17:17:40 +0200

    correct login

--- StandUpTimer/Services/Server.cs
+++ StandUpTimer/Services/Server.cs
@@ -1,7 +1,11 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Runtime.InteropServices;
 using System.Security;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using StandUpTimer.Web.Contract;
 
@@ -9,25 +13,47 @@ namespace StandUpTimer.Services
 {
     internal class Server : IServer
     {
+        private const string BaseUrl = "http://localhost:54776";
+
         private readonly HttpClient httpClient;
+        private readonly CookieContainer cookies;
 
         public Server()
         {
-            httpClient = new HttpClient { BaseAddress = new Uri("http://localhost:54776") };
+            cookies = new CookieContainer();
+
+            var handler = new HttpClientHandler
+            {
+                CookieContainer = cookies,
+                UseCookies = true
+            };
+
+            httpClient = new HttpClient(handler)
+            {
+                BaseAddress = new Uri(BaseUrl),
+            };
         }
 
         public void SendDeskState(Status status)
         {
-            httpClient.PostAsJsonAsync("statistics", status);
+            var foo = httpClient.PostAsJsonAsync("statistics", status).Result;
+            var bar = foo;
         }
 
         public async Task<bool> LogIn(string username, SecureString password)
         {
-            var response = await httpClient.PostAsJsonAsync("Account/Login", new
+            var returnedSite = await httpClient.GetStringAsync("Account/Login");
+            const string pattern = @"<input name=""__RequestVerificationToken"" type=""hidden"" value=""(?<token>.*)"" />";
+            var token = Regex.Match(returnedSite, pattern).Groups["token"].Value;
+
+            var content = new FormUrlEncodedContent(new[]
             {
-                Email = username,
-                Password = password.ConvertToUnsecureString()
+                new KeyValuePair<string, string>("UserName", username),
+                new KeyValuePair<string, string>("Password", password.ConvertToUnsecureString()),
+                new KeyValuePair<string, string>("__RequestVerificationToken", token)
             });
+
+            var response = await httpClient.PostAsync("Account/Login", content);
 
             return response.IsSuccessStatusCode;
         }
--- StandUpTimer/Views/Dialog.xaml.cs
+++ StandUpTimer/Views/Dialog.xaml.cs
@@ -7,16 +7,19 @@ namespace StandUpTimer.Views
     {
         public Dialog(IDialogViewModel dataContext)
         {
-            WeakEventManager<IDialogViewModel, RequestCloseEventArgs>.AddHandler(dataContext, "RequestClose",
-                (sender, args) =>
-                {
-                    DialogResult = args.DialogResult;
-                    Close();
-                });
+            WeakEventManager<IDialogViewModel, RequestCloseEventArgs>.AddHandler(dataContext, "RequestClose", OnRequestClose);
 
             DataContext = dataContext;
 
             InitializeComponent();
+        }
+
+        private void OnRequestClose(object sender, RequestCloseEventArgs e)
+        {
+            DialogResult = e.DialogResult;
+            Close();
+
+            WeakEventManager<IDialogViewModel, RequestCloseEventArgs>.RemoveHandler(DataContext as IDialogViewModel, "RequestClose", OnRequestClose);
         }
     }
 }
