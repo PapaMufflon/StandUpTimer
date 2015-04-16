6efea7de1d36997807e03fa24943bc373c72206b
Tobias Walter <tobias.walter@gmx.net>
2015-04-16 09:21:18 +0200

    use semantic versioning instead of automatically increment the version number

--- StandUpTimer/Properties/AssemblyInfo.cs
+++ StandUpTimer/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyVersion("0.6.0416.31")]
-[assembly: AssemblyFileVersion("0.6.0416.31")]
+[assembly: AssemblyVersion("0.7.0")]
+[assembly: AssemblyFileVersion("0.7.0.*")]
 
 [assembly: AssemblyMetadata("SquirrelAwareVersion", "1")]
 
