5742ac5f8abb9d339ef84a643049b765569f46f6
Tobias Walter <tobias.walter@gmx.net>
2015-04-16 10:37:47 +0200

    remove wildcards in AssemblyFileVersion

--- StandUpTimer/Properties/AssemblyInfo.cs
+++ StandUpTimer/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 
 [assembly: AssemblyVersion("0.7.0")]
-[assembly: AssemblyFileVersion("0.7.0.*")]
+[assembly: AssemblyFileVersion("0.7.0")]
 
 [assembly: AssemblyMetadata("SquirrelAwareVersion", "1")]
 
