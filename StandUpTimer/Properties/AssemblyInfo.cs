3f93633b303d518feb6f1fe0981f0f6b77b2b8a9
Tobias Walter <tobias.walter@gmx.net>
2015-04-27 15:53:08 +0200

    version bump desktop app

--- StandUpTimer/Properties/AssemblyInfo.cs
+++ StandUpTimer/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyVersion("0.7.1")]
-[assembly: AssemblyFileVersion("0.7.1")]
+[assembly: AssemblyVersion("0.7.2")]
+[assembly: AssemblyFileVersion("0.7.2")]
 
 [assembly: InternalsVisibleTo("StandUpTimer.UnitTests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
