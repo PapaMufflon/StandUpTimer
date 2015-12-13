a63e3b2d9b3020f0dd3caaa3c4c65b885c13c786
Tobias Walter <tobias.walter@gmx.net>
2015-12-13 14:37:25 +0100

    increase version number / just a test if update works

--- StandUpTimer/Properties/AssemblyInfo.cs
+++ StandUpTimer/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyVersion("0.8.2")]
-[assembly: AssemblyFileVersion("0.8.2")]
+[assembly: AssemblyVersion("0.8.3")]
+[assembly: AssemblyFileVersion("0.8.3")]
 
 [assembly: InternalsVisibleTo("StandUpTimer.UnitTests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
